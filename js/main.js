378a12829df31da96ccccc817009bda41609690e
yukimemi <yukimemi@gmail.com>
2014-06-21 00:10:43 +0900

    compress and deploy

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(function(){console.log("hello from a require'd coffee file <-- via assets/js/_helper.coffee")}).call(this),function(){var a;a=["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"],require(a,function(){return $(function(){return console.log("jquery loaded, dom ready <-- via assets/js/main.coffee")})})}.call(this)
